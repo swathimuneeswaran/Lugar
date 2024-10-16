dbe708428a98c0f1852e0e2fdd86b7d86fcd3317
SWATHI M <113039047+swathimuneeswaran@users.noreply.github.com>
2024-10-16 13:39:54 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,3 @@
-// import { Route, Routes } from "react-router-dom";
-// import Navbar from "./components/Navbar/Navbar";
-// import NavbarBanner from "./components/Navbar/NavbarBanner";
-// import Home from "./components/Home/Home";
-// import WebDevelopment from "./pages/web-development";
-// import Footer from "./components/Footer/Footer";
-// import Testimonial from "./components/Testimonial/Testimonial";
-// import Digitalpage from "./pages/Digitalmarketing/Digitalpage";
-// import Businessconsulting from "./pages/Business-consulting/Business-consulting";
-// import Aboutus from "./pages/About-us/About-us";
-// import Contactus from "./pages/Contact-us/Contactus";
-
-// const App = () => {
-//   return (
-//     <main className="overflow-x-hidden">
-//       <NavbarBanner />
-//       <Navbar />
-//       <Routes>
-//         <Route index path="/" element={<Home />} />
-//         <Route path="/services/web-development" element={<WebDevelopment />} />
-//         <Route path="/services/digital-marketing" element={<Digitalpage />} />
-//         <Route path="/services/business-consulting" element={<Businessconsulting />} />
-//         <Route path="/about-us" element={<Aboutus />} />
-//       </Routes>
-//       <Contactus/>
-//       <Testimonial />
-//       <Footer />
-//     </main>
-//   );
-// };
-
-// export default App;
-
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import NavbarBanner from "./components/Navbar/NavbarBanner";
@@ -40,7 +7,7 @@ import Footer from "./components/Footer/Footer";
 import Testimonial from "./components/Testimonial/Testimonial";
 import Digitalpage from "./pages/Digitalmarketing/Digitalpage";
 import Businessconsulting from "./pages/Business-consulting/Business-consulting";
-import Aboutus from "./pages/About-us/Aboutus";
+import Aboutus from "./pages/About-us/About-us";
 import Contactus from "./pages/Contact-us/Contactus";
 
 const App = () => {
@@ -63,4 +30,3 @@ const App = () => {
 };
 
 export default App;
-
